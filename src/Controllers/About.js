e1c27fc1fc3c78e26bc1d5a12295b9749dfc67a9
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-26 23:58:36 +0500

    Add View About page

--- src/Controllers/About.js
+++ src/Controllers/About.js
@@ -1,7 +1,8 @@
 const about = require('./../Models/about')
+const path = require('path')
 
 const GET = (req, res) => {
-	res.send(about)
+	res.sendFile(path.join(__dirname, '../View', '/about.html'))
 }
 
 module.exports.GET = GET
